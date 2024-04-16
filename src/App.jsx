ef9592266e127e70cfb92e67efa1b8fc1a8966d5
francobustos23 <bustosfranco071@gmail.com>
2024-04-16 16:07:15 -0300

    Se agrego el boton para cambiar el pokemon a shainy

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import "../public/App.css";
 function App() {
   const [pokemones, setPokemones] = useState([]);
   const [mostrarLista, setMostrarLista] = useState(false);
+  const [pokemonShiny, setPokemonShiny] = useState({});
 
   useEffect(() => {
     const getPokemones = async () => {
-      //Obtenemos la lista de pokemones de la api
       const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
       const listaPokemones = await response.json();
       const { results } = listaPokemones;
 
-      /*
-        Obtenemos la otra url para hacer un fetch y asi 
-        acceder al id, nombre, imagen y tipo de cada pokemon.
-      */
       const newPokemones = results.map(async (pokemon) => {
         const response = await fetch(pokemon.url);
         const poke = await response.json();
@@ -23,22 +19,30 @@ function App() {
           id: poke.id,
           name: poke.name,
           img: poke.sprites.other.dream_world.front_default,
+          shiny: poke.sprites.other["official-artwork"].front_shiny,
           type: poke.types,
         };
       });
-      //resolvemos las promesas
+
       setPokemones(await Promise.all(newPokemones));
     };
     getPokemones();
   }, []);
-  //esta funcion cambia el estado cada vez que se hace click
+
   const toggleLista = () => {
     setMostrarLista(!mostrarLista);
   };
-  //esta funcion elimina el pokemon del estado pokemones
+
   const handleDeletePokemon = (id) => {
     const updatedPokemones = pokemones.filter((pokemon) => pokemon.id !== id);
     setPokemones(updatedPokemones);
+  };
+
+  const cambiar_a_shiny = (id) => {
+    setPokemonShiny((prev) => ({
+      ...prev,
+      [id]: !prev[id], // Cambiar el estado opuesto al estado actual
+    }));
   };
 
   return (
@@ -56,20 +60,21 @@ function App() {
         </button>
       </div>
 
-
       {mostrarLista && (
         <div className="listpoke">
           {pokemones.map((pokemon) => {
             return (
               <div key={pokemon.id} className="list">
-                <img src={pokemon.img} alt={pokemon.name} />
+                <img
+                  src={pokemonShiny[pokemon.id] ? pokemon.shiny : pokemon.img} // Verifica si el Pokémon debe mostrarse en versión shiny
+                  alt={pokemon.name}
+                  id="img-pokemon"
+                />
                 <div>
                   <h2>{pokemon.name}</h2>
-                  <span>{pokemon.id}</span>
-                  <h3>
-                    Type:{" "}
-                    {pokemon.type.map((type) => type.type.name).join(", ")}
-                  </h3>
+                  {/* <span>{pokemon.id}</span> */}
+                  <h3>Type: {pokemon.type.map((type) => type.type.name).join(", ")}</h3>
+                  <button onClick={() => cambiar_a_shiny(pokemon.id)} className="boton-eli">{pokemonShiny[pokemon.id] ? "⏪ Normal" : "Shiny ⏩"}</button>
                   <button onClick={() => handleDeletePokemon(pokemon.id)} className="boton-eli">
                     Eliminar
                   </button>
