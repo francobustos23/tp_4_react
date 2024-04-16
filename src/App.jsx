b941a0ffe343d5cea3f8dd808089dbb75b246d1c
francobustos23 <bustosfranco071@gmail.com>
2024-04-16 09:29:29 -0300

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import React, { useEffect, useState } from "react";
-import "../public/App.css"
+import { useEffect, useState } from "react";
+import "../public/App.css";
+
 function App() {
-  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemones, setPokemones] = useState([]);
+  const [mostrarLista, setMostrarLista] = useState(false);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
-      .then((data) => data.json())
-      .then((datos) => {
-        setPokemonData(datos.results);
-      })
-      .catch((err) => {
-        console.log("Error al traer los datos: ", err);
+    const getPokemones = async () => {
+      //Obtenemos la lista de pokemones de la api
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+      const listaPokemones = await response.json();
+      const { results } = listaPokemones;
+
+      /*
+        Obtenemos la otra url para hacer un fetch y asi 
+        acceder al id, nombre, imagen y tipo de cada pokemon.
+      */
+      const newPokemones = results.map(async (pokemon) => {
+        const response = await fetch(pokemon.url);
+        const poke = await response.json();
+        return {
+          id: poke.id,
+          name: poke.name,
+          img: poke.sprites.other.dream_world.front_default,
+          type: poke.types,
+        };
       });
+      //resolvemos las promesas
+      setPokemones(await Promise.all(newPokemones));
+    };
+    getPokemones();
   }, []);
+  //esta funcion cambia el estado cada vez que se hace click
+  const toggleLista = () => {
+    setMostrarLista(!mostrarLista);
+  };
+  //esta funcion elimina el pokemon del estado pokemones
+  const handleDeletePokemon = (id) => {
+    const updatedPokemones = pokemones.filter((pokemon) => pokemon.id !== id);
+    setPokemones(updatedPokemones);
+  };
 
   return (
-    <div>
+    <div className="custom-cursor">
       <div className="title">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt=""  width={"500px"}/>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png"
+          alt=""
+          width={"500px"}
+        />
       </div>
-      <ul>
-        {pokemonData.map((pokemon, index) => (
-          <li key={index}>
-            <h3>Nombre: {pokemon.name}</h3>
-            <a href={pokemon.url}>Mas información</a>
-            {/* <p onClick={}>Eliminar</p> */}
-            <p>Eliminar</p>
-          </li>
-        ))}
-      </ul>
-      
+      <div className="boton">
+        <button onClick={toggleLista} className="boton-color">
+          {mostrarLista ? "Ocultar Pokemones" : "Mostrar Pokemones"}
+        </button>
+      </div>
+
+
+      {mostrarLista && (
+        <div className="listpoke">
+          {pokemones.map((pokemon) => {
+            return (
+              <div key={pokemon.id} className="list">
+                <img src={pokemon.img} alt={pokemon.name} />
+                <div>
+                  <h2>{pokemon.name}</h2>
+                  <span>{pokemon.id}</span>
+                  <h3>
+                    Type:{" "}
+                    {pokemon.type.map((type) => type.type.name).join(", ")}
+                  </h3>
+                  <button onClick={() => handleDeletePokemon(pokemon.id)} className="boton-eli">
+                    Eliminar
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
